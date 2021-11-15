7d5ae290b9f5c107e6c90717dfe3f4f088392bce
huangwei4478 <huangwei4478@users.noreply.github.com>
2021-11-16 00:00:58 +0800

    [add] Finish "Game Class"

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
-#include <glm/glm.hpp>
-#include <imgui/imgui.h>
-#include <sol/sol.hpp>
+
+
 
 int main(int argc, char *argv[]) {
+	Game game;
 
-    sol::state lua;
-    lua.open_libraries(sol::lib::base);
+	game.Initialize();
 
-    glm::vec2 velocity = glm::vec2(2.0, -1.0);
+	game.Run();
 
-    SDL_Init(SDL_INIT_EVERYTHING);
-    std::cout << "Dependencies work correctly." << std::endl;
-    return 0;
+	game.Destroy();
+
+	return 0;
 }
